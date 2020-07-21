7f38637b665d6f1e285f0cded8cafd825d33bee4
hobo-clh <807255841@qq.com>
2020-07-22 00:09:44 +0800

    修复页面出现空标签的bug

---
+++ src/test/java/com/ccsu/community/FileTest.java
@@ -0,0 +1,8 @@
+package com.ccsu.community;
+
+/**
+ * @author: LongHua
+ * @date: 2020/7/9
+ **/
+public class FileTest {
+}
---
+++ src/test/java/com/ccsu/community/MVCTest.java
@@ -0,0 +1,8 @@
+package com.ccsu.community;
+
+/**
+ * @author 华华
+ * @date 20/06/11
+ **/
+public class MVCTest {
+}
